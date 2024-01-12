2b6c024857fbb7392dae9e7cb30aea87e14329ea
Daniel Comaduran <38022865+danielcomaduran@users.noreply.github.com>
2024-01-11 17:28:28 -0700

    Object to work with temple data

---
+++ Functions/temple_data.py
@@ -0,0 +1,35 @@
+
+import mne.io as io
+import numpy as np
+
+class TempleData:
+    """ Object to handle the Temple Artifact Dataset. """
+    def __init__(
+        self,
+        file_name:str,
+        montage_type:str,
+        ):
+    
+        self.file_name = file_name
+        self.montage_type = montage_type
+    
+        # Create EDF object with data
+        # self.edf = io.edf(self.file_name)
+
+    def set_montage(self):
+        """ Creates an array mask to obtain the proper montage """
+
+    def get_data(self):
+        """ Returns full trial with proper montage set. """
+
+    def get_clean_data(self):
+        """ Returns a list of windows with all the windows that are not
+            asociated to an artifact (i.e., clean windows). """
+
+    def get_artifacts(self, artifact_type):
+        """ Returns a list with all the artifacts of type `artifact_type` """
+
+    def list_to_np(self, data:list):
+        """ Trims `data` list to the shortest element and returns a numpy
+            array where the first dimension = len(data). """
+
