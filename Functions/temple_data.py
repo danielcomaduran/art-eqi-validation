93ec25639ef531baf4cdbae034fab8db2e408c11
Daniel Comaduran <38022865+danielcomaduran@users.noreply.github.com>
2024-01-17 13:33:00 -0700

    Fixed typo in montage dict

--- Functions/temple_data.py
+++ Functions/temple_data.py
@@ -49,27 +49,27 @@ class TempleData:
 
             "le": {
                 "FP1-F7": "EEG FP1-LE - EEG F7-LE",
-                "F7-T3":  "EEG F7-LE  - EEG T3-LE",
-                "T3-T5":  "EEG T3-LE  - EEG T5-LE",
-                "T5-O1":  "EEG T5-LE  - EEG O1-LE",
+                "F7-T3":  "EEG F7-LE - EEG T3-LE",
+                "T3-T5":  "EEG T3-LE - EEG T5-LE",
+                "T5-O1":  "EEG T5-LE - EEG O1-LE",
                 "FP2-F8": "EEG FP2-LE - EEG F8-LE",
-                "F8-T4":  "EEG F8-LE  - EEG T4-LE",
-                "T4-T6":  "EEG T4-LE  - EEG T6-LE",
-                "T6-O2":  "EEG T6-LE  - EEG O2-LE",
-                "A1-T3":  "EEG A1-LE  - EEG T3-LE",
-                "T3-C3":  "EEG T3-LE  - EEG C3-LE",
-                "C3-CZ":  "EEG C3-LE  - EEG CZ-LE",
-                "CZ-C4":  "EEG CZ-LE  - EEG C4-LE",
-                "C4-T4":  "EEG C4-LE  - EEG T4-LE",
-                "T4-A2":  "EEG T4-LE  - EEG A2-LE",
+                "F8-T4":  "EEG F8-LE - EEG T4-LE",
+                "T4-T6":  "EEG T4-LE - EEG T6-LE",
+                "T6-O2":  "EEG T6-LE - EEG O2-LE",
+                "A1-T3":  "EEG A1-LE - EEG T3-LE",
+                "T3-C3":  "EEG T3-LE - EEG C3-LE",
+                "C3-CZ":  "EEG C3-LE - EEG CZ-LE",
+                "CZ-C4":  "EEG CZ-LE - EEG C4-LE",
+                "C4-T4":  "EEG C4-LE - EEG T4-LE",
+                "T4-A2":  "EEG T4-LE - EEG A2-LE",
                 "FP1-F3": "EEG FP1-LE - EEG F3-LE",
-                "F3-C3":  "EEG F3-LE  - EEG C3-LE",
-                "C3-P3":  "EEG C3-LE  - EEG P3-LE",
-                "P3-O1":  "EEG P3-LE  - EEG O1-LE",
+                "F3-C3":  "EEG F3-LE - EEG C3-LE",
+                "C3-P3":  "EEG C3-LE - EEG P3-LE",
+                "P3-O1":  "EEG P3-LE - EEG O1-LE",
                 "FP2-F4": "EEG FP2-LE - EEG F4-LE",
-                "F4-C4":  "EEG F4-LE  - EEG C4-LE",
-                "C4-P4":  "EEG C4-LE  - EEG P4-LE",
-                "P4-O2":  "EEG P4-LE  - EEG O2-LE"
+                "F4-C4":  "EEG F4-LE - EEG C4-LE",
+                "C4-P4":  "EEG C4-LE - EEG P4-LE",
+                "P4-O2":  "EEG P4-LE - EEG O2-LE"
             },
 
             "ar_a": {
@@ -108,8 +108,8 @@ class TempleData:
             minuend = value.split(" - ")[0]
             subtrahend = value.split(" - ")[1]
             
-            iminuend = [i for [i, channel] in enumerate(self.ch_names) if channel == minuend]
-            isubtrahend = [i for [i, channel] in enumerate(self.ch_names) if channel == subtrahend]
+            iminuend = [i for [i, channel] in enumerate(self.ch_names) if channel == minuend][0]
+            isubtrahend = [i for [i, channel] in enumerate(self.ch_names) if channel == subtrahend][0]
             
             transformed_data[c,:] = self.data[isubtrahend,:] - self.data[iminuend,:]
             
