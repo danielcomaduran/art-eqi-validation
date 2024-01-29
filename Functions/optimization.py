63e6432d80155a1ec2b9186f37328f89facb5ff7
Daniel Comaduran <38022865+danielcomaduran@users.noreply.github.com>
2024-01-29 14:40:09 -0700

    Fixed window size for ART

--- Functions/optimization.py
+++ Functions/optimization.py
@@ -22,7 +22,7 @@ def maximize_eqi(x, *args):
 
     # Creat artifact removal object
     art = ART(
-        window_length = window_samples,
+        window_length = (window_samples // 2),  # To have 500 msec windows like the paper
         n_clusters = n_clusters,
         fd_threshold = fd_threshold,
         ssa_threshold = ssa_threshold,
