b71a9ae45d9c0c2898cf4b93a2b090c92ab21af4
Daniel Comaduran <38022865+danielcomaduran@users.noreply.github.com>
2024-01-29 13:09:57 -0700

    Added Araz optimization file

--- Functions/optimization.py
+++ Functions/optimization.py
@@ -1,7 +1,10 @@
 ## Import libraries
 # - Default libraries
+import copy
 import numpy as np
 import scipy.optimize as optimize
+from rich import print as rprint
+from rich.pretty import pprint as rpprint
 
 # Custom libraries
 from Functions.artifact_removal_tool import ART
@@ -9,8 +12,8 @@ import Functions.eeg_quality_index as eqi
 
 def maximize_eqi(x, *args):
     # Separate input variables
-    [n_clusters, fd_threshold, ssa_threshold] = x
-    [clean_data, artifact_data, srate, window_length] = args
+    [n_clusters, fd_threshold, ssa_threshold] = copy.deepcopy(x)
+    [clean_data, artifact_data, srate, window_length, var_tol] = args
 
     n_clusters = int(n_clusters)
 
@@ -19,30 +22,40 @@ def maximize_eqi(x, *args):
 
     # Creat artifact removal object
     art = ART(
-        window_length = window_samples // 2,
+        window_length = window_length,
         n_clusters = n_clusters,
         fd_threshold = fd_threshold,
-        ssa_threshold = ssa_threshold 
-        )
+        ssa_threshold = ssa_threshold,
+        var_tol = var_tol
+    )
 
     # Apply artifact removal
     test_data = art.remove_artifacts(
         eeg_data = artifact_data,
         srate = srate
-        )
+    )
 
-    eqi_total = eqi.scoring(
+    eqi_scoring = eqi.scoring(
         clean_eeg = clean_data,
         test_eeg = test_data,
         srate_clean = srate,
         srate_test = srate,
-        window = int(srate),
-        slide = int(srate // 10)
-        )[0]
+        window = int(window_samples // 10),
+        slide = int(window_samples // 20)
+    )
+
+    # rpprint({
+    #     "n_clusters": n_clusters,
+    #     "fd_threshold": fd_threshold,
+    #     "ssa_threshold": ssa_threshold,
+    #     "eqi_scoring[0]": eqi_scoring[0]
+    # })
+
+    eqi_total = eqi_scoring[0]
 
     # Use the complement to minimize the problem
     eqi_total_complement = 100 - np.mean(eqi_total)
-    print(f" EQI value {eqi_total_complement}")
+    # print(f" EQI value {eqi_total_complement}")
 
     return eqi_total_complement
 
